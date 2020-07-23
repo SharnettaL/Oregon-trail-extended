b5349e6db6cbf02bc69fa5338be6dfb91ac27372
Sharnetta <sharnettalamar@yahoo.com>
2020-07-23 18:50:39 -0400

    extended

--- travelers.js
+++ travelers.js
@@ -14,3 +14,36 @@ eat(){
     this.food -= 1
 }
 }
+class Doctor extends Traveler{
+    constructor(name){
+        super(name)
+    }
+heal(Traveler){
+    this.isHealthy = false
+}
+}
+
+class Hunter extends Traveler {
+    constructor(name){
+        super(name)
+     this.food = 2
+
+    }
+hunt(){
+    this.food += 5
+}
+eat(){
+    if(this.food <= 1 ){
+        return this.isHealthy = false
+    }
+    this.food -= 2
+}
+giveFood(Traveler, numOfFoodUnits){
+ numOfFoodUnits = 2
+ if(this.food >= numOfFoodUnits){
+     this.food - numOfFoodUnits
+ }
+ }
+
+}
+
